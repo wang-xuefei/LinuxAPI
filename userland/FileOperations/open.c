bc850eee6f465a309be0a2179e901719098cfe31
wangxuefei <wangxuefei@geyecloud.com>
2019-05-06 16:05:21 +0800

    no message

--- userland/FileOperations/open.c
+++ userland/FileOperations/open.c
@@ -3,14 +3,27 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
-int main(int argc, char **argv)
+
+int create_file(const char *path)
 {
 	int fd = 0;
-
-	fd = open(argv[1], O_CREATE|O_TRUNC|O_RDWR, 0644);
+	
+	fd = open(path, O_CREATE|O_TRUNC|O_RDWR, 0644);
 	if(fd < 0){
 		printf("open %s is error!\n", argv[1]);
 	}
-	
+
+	return fd;
+}
+
+int main(int argc, char **argv)
+{
+	int fd;
+
+	fd = create_file(argv[1]);
+
 	return 0;
 }
+
+
+
